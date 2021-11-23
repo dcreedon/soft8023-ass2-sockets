5387673d3aec9fed38c412c4c0e30beafae1c6b6
dcreedon <damian.creedon@gmail.com>
2021-11-23 17:24:21 +0000

    Added RABBITHOST constant for RabbitMQ connection

--- app/server/server.py
+++ app/server/server.py
@@ -6,7 +6,7 @@ import pika
 LOCALHOST = "0.0.0.0"           # to allow docker to bind to external port
 PORT = 64002
 
-RABBITHOST = "rabbitmq"
+RABBITHOST = "0.0.0.0"
 
 class ClientThread(threading.Thread):
 
